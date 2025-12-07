1bcd0f1d8cf5f7f106f40b43c9bd58775911f67a
Raul Contreras <raul90c@gmail.com>
2025-12-07 15:29:36 -0600

    test: agregar estructura de test suma

---
+++ test_calc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+const double EPSILON = 0.001;
+
+void testSuma() {
+    cout << "Test Suma: Pending << endl;
+}
+
+int main()  {
+cout << "Ejecutando pruebas de la Calculadora..." << endl;
+testSuma();
+return 0;
+}
+
